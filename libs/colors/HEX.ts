a06f4da7e1c2fb4b8d2f6b3dd8cef5942a87f973
Alikuxac <lexson270400@gmail.com>
2023-08-31 14:32:59 +0700

    feat: add color classes

---
+++ libs/colors/HEX.ts
@@ -0,0 +1,62 @@
+import type { ColorHandler } from "./index";
+
+export class HEX implements ColorHandler {
+  public r: string;
+  public g: string;
+  public b: string;
+
+  /**
+   * HEX Parser.
+   * @param r Red value. ('00' - 'ff')
+   * @param g Green value. ('00' - 'ff')
+   * @param b Blue value. ('00' - 'ff')
+   */
+  public constructor(r: string, g: string, b: string) {
+    this.r = r.padStart(2, "0");
+    this.g = g.padStart(2, "0");
+    this.b = b.padStart(2, "0");
+
+    this.check();
+  }
+
+  public check() {
+    if (Number.isNaN(parseInt(this.r, 16))) {
+      throw new Error("Invalid hex red value");
+    }
+    if (Number.isNaN(parseInt(this.g, 16))) {
+      throw new Error("Invalid hex green value");
+    }
+    if (Number.isNaN(parseInt(this.b, 16))) {
+      throw new Error("Invalid hex blue value");
+    }
+  }
+
+  public get hex() {
+    return this;
+  }
+
+  public get rgb() {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    return new RGB(
+      parseInt(this.r, 16),
+      parseInt(this.g, 16),
+      parseInt(this.b, 16),
+    );
+  }
+
+  public get hsl() {
+    return this.rgb.hsl;
+  }
+
+  public get b10() {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    return new B10(parseInt(this.r + this.g + this.b, 16));
+  }
+
+  public toString() {
+    return `#${this.r}${this.g}${this.b}`;
+  }
+}
+
+import { B10 } from "./B10";
+import { RGB } from "./RGB";
